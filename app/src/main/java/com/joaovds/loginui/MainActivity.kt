00a07106694df71b40f3546202fd26e5de2198d9
João Victor da Silva <jv782063@gmail.com>
2025-01-10 11:56:33 -0300

    create registration screen

--- app/src/main/java/com/joaovds/loginui/MainActivity.kt
+++ app/src/main/java/com/joaovds/loginui/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.joaovds.loginui.ui.screen.LoginScreen
+import com.joaovds.loginui.ui.screen.RegistrationScreen
 import com.joaovds.loginui.ui.theme.LoginUITheme
 
 class MainActivity : ComponentActivity() {
@@ -14,8 +14,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             LoginUITheme {
                 // WelcomeScreen()
-                LoginScreen()
-                // RegistrationScreen()
+                // LoginScreen()
+                RegistrationScreen()
             }
         }
     }
--- app/src/main/java/com/joaovds/loginui/ui/screen/Registration.kt
+++ app/src/main/java/com/joaovds/loginui/ui/screen/Registration.kt
@@ -1,9 +1,37 @@
 package com.joaovds.loginui.ui.screen
 
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.joaovds.loginui.R
+import com.joaovds.loginui.ui.components.AuthenticationScreenTemplate
+import com.joaovds.loginui.ui.theme.PrimaryPinkDark
+import com.joaovds.loginui.ui.theme.PrimaryViolet
+import com.joaovds.loginui.ui.theme.PrimaryVioletDark
+import com.joaovds.loginui.ui.theme.PrimaryVioletLight
 
 @Composable
 fun RegistrationScreen(modifier: Modifier = Modifier) {
-
+    AuthenticationScreenTemplate(
+        modifier = modifier,
+        backgroundGradient = arrayOf(0f to PrimaryViolet, 1f to PrimaryVioletDark),
+        imgRes = R.drawable.img_coder_w,
+        title = "Olá, bem vindo!",
+        subtitle = "Vamos Começar",
+        mainActionButtonTitle = "Criar Conta",
+        secondaryActionButtonTitle = "Login",
+        mainActionButtonColors = ButtonDefaults.buttonColors(
+            contentColor = Color.White,
+            containerColor = PrimaryVioletDark
+        ),
+        secondaryActionButtonColors = ButtonDefaults.buttonColors(
+            contentColor = Color.White,
+            containerColor = PrimaryVioletLight
+        ),
+        mainActionButtonShadow = PrimaryPinkDark,
+        secondaryActionButtonShadow = PrimaryPinkDark,
+        onMainActionButtonClicked = {},
+        onSecondaryActionButtonClicked = {}
+    )
 }
